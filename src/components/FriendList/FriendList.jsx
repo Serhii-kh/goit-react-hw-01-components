dda64c94c065f83fc0269de2ed1a6c59bacac59c
Serhii Naboka <drifterzzz@gmail.com>
2023-04-23 18:08:11 +0300

    start adding css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css.friend__list}>
       {friends.map(({ avatar, name, id, isOnline }) => {
         return (
-          <li key={id} className="item">
-            <span className="status"></span>
-            <img className="avatar" src={avatar} alt="User avatar" width="48" />
-            <p className="name">{name}</p>
+          <li key={id} className={css.item}>
+            <span className={css.status}></span>
+            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+            <p className={css.name}>{name}</p>
           </li>
         );
       })}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,17 +1,17 @@
 import css from 'components/Statistics/Statistics.module.css';
 import PropTypes from 'prop-types';
-import getRandomHexColor from '../../js/getRandomHexColor.js'
+import getRandomHexColor from '../../js/getRandomHexColor.js';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.stat__list}>
         {stats.map(({ id, label, percentage }) => {
           return (
-            <li key={id} className="item">
-              <span className="label">{label}</span>
-              <span className="percentage">{percentage}</span>
+            <li key={id} className={css.item}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
             </li>
           );
         })}
