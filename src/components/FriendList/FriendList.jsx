204d497ad7df88deff8718341dd9e6897265899a
Serhii Naboka <drifterzzz@gmail.com>
2023-04-25 19:26:23 +0300

    upd

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -9,7 +9,12 @@ export const FriendList = ({ friends }) => {
         return (
           <li key={id} className={css.item}>
             <span className={css.status}></span>
-            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+            <img
+              className={css.avatar}
+              src={avatar}
+              alt="User avatar"
+              width="48"
+            />
             <p className={css.name}>{name}</p>
           </li>
         );
