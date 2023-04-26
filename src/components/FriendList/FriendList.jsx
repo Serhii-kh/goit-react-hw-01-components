fc5e33e739f8cce2f7d5562e2377536d35ad0341
Serhii Naboka <drifterzzz@gmail.com>
2023-04-26 17:45:28 +0300

    upd

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,27 +1,13 @@
 import css from 'components/FriendList/FriendList.module.css';
-// import { FriendListItem } from 'components/FriendListItem/FriendListItem.jsx';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem.jsx';
 import PropTypes from 'prop-types';
-import { checkIsOnline } from '../../js/checkIsOnline';
 
 export const FriendList = ({ friends }) => {
+  const { avatar, name, isOnline, id } = friends;
   return (
     <ul className={css.friend__list}>
-      {friends.map(({ avatar, name, id, isOnline }) => {
-        return (
-          <li key={id} className={css.item}>
-            <span
-              className={css.status}
-              style={{ background: checkIsOnline(isOnline) }}
-            ></span>
-            <img
-              className={css.avatar}
-              src={avatar}
-              alt="User avatar"
-              width="48"
-            />
-            <p className={css.name}>{name}</p>
-          </li>
-        );
+      {friends.map((friend)=> {
+        return <FriendListItem key={id}/>;
       })}
     </ul>
   );
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,13 @@
 import css from 'components/FriendList/FriendList.module.css';
-// import PropTypes from 'prop-types';
+import { checkIsOnline } from 'js/checkIsOnline';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status}></span>
+      <span
+        className={css.status}
+        style={{ background: checkIsOnline(isOnline) }}
+      ></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
